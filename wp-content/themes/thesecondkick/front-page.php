5a90d1ea2ef40e2d095192a1d8bb67934fc2de49
Amandeep Singh <samandeep8057@gmail.com>
2024-07-01 16:55:26 +0530

    resolved images getting weired space at the end heroku

--- wp-content/themes/thesecondkick/front-page.php
+++ wp-content/themes/thesecondkick/front-page.php
@@ -3,9 +3,7 @@ get_header()
 ?>
 
 <!-- ***************** Grabing Content from WP -> (custom_content_setup_menu page) ***************** -->
-
 <?php 
-
 $heroku_headline = get_option('heroku_headline');
 $heroku_paragraph = get_option('heroku_paragraph');
 $heroku_img_url = get_option('heroku_img_url');
@@ -13,12 +11,8 @@ $heroku_association1_img_url = get_option('heroku_association1_img_url');
 $heroku_association2_img_url = get_option('heroku_association2_img_url');
 $heroku_association3_img_url = get_option('heroku_association3_img_url');
 $heroku_association4_img_url = get_option('heroku_association4_img_url');
-
 ?>
-
-
 <!-- ***************** Got the content ***************** -->
-
 
 <!-- heroku section -->
 <!-- {navbar[12]}[5%] -->
@@ -26,16 +20,20 @@ $heroku_association4_img_url = get_option('heroku_association4_img_url');
 <!-- {space[1-0]---- assocs[8-12] ----space[1-0]}[35%] -->
 <!-- heroku section -->
 
+<!-- ********* main-section ********* -->
 <main class="container   mb-20" id="content">
+  <!-- heroku-section -->
   <div class="heroku-container lg:mt-20 md:mt-10 sm:mt-5 grid  gap-5  sm:gap-[96px]">
     <!-- grid-rows-2 use it to make two colms-->
-
+    <!-- heroku-section-row1 -->
     <div
       class="heroku-container__row1-container flex flex-col lg:flex-row lg:gap-col md:gap-col-0.5 justify-between items-center bg-gradient-to-tr from-grad-red-0 to-grad-red-100 sm:bg-none text-white sm:text-primary py-8 sm:py-0 ">
-
-      <div class="order-1 lg:order-2 lg:w-8/12 md:w-full tagline-img-container flex justify-end hidden sm:block">
-        <img src="<?php echo $heroku_img_url; ?>" alt="district-league-team-photo" class="rounded-lg">
+      <!-- heroku-section-row1 > img-container -->
+      <div class="order-1 lg:order-2 lg:w-8/12 md:w-full tagline-img-container flex justify-end">
+        <img src="<?php echo $heroku_img_url; ?>" alt="district-league-team-photo" class="rounded-lg hidden sm:block">
       </div>
+      <!-- heroku-section-row1 > img-container ends here -->
+      <!-- heroku-section-row1 > text-container -->
       <div class="order-2 lg:order-1 lg:w-4/12 md:w-8/12 m-3  tagline-container flex   flex-col md:gap-3 gap-2 ">
         <h1 class="sm:text-5xl text-center  lg:text-left text-3xl tracking-wide  lg:tracking-tight font-semibold">
           <?php echo $heroku_headline; ?></h1>
@@ -50,28 +48,12 @@ $heroku_association4_img_url = get_option('heroku_association4_img_url');
           </button>
         </div>
       </div>
+      <!-- heroku-section-row1 > img-container ends here -->
     </div>
-    <!-- https://www.firstkickschoolofsoccer.com/wp-content/uploads/2023/05/fkss-logo-93x93.png -->
-    <!-- <div class="heroku-container__row2-container flex items-end justify-center items-center">
-      <div
-        class="association-container lg:w-6/12 w-8/12 sm:width-full  flex sm:flex-row flex-col gap-5 lg:gap-[96px] items-center">
-        <div class=" flex items-center">
-          <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/aiff_logo.png"
-            alt="district-league-team-photo">
-        </div>
-        <div class="  flex items-center">
-          <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/ghaziabad_fa_logo.jpg"
-            alt="district-league-team-photo">
-        </div>
-        <div class="  flex items-center">
-          <img src="https://www.firstkickschoolofsoccer.com/wp-content/uploads/2023/05/fkss-logo-93x93.png"
-            alt="district-league-team-photo">
-        </div>
-        <div class="  flex items-center">
-          <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/logo_upfs.jpg">
-        </div>
-      </div> -->
+    <!-- heroku-section-row1 ends here -->
+    <!-- heroku-section-row2 -->
     <div class="heroku-container__row2-container flex items-end justify-center items-center">
+      <!-- heroku-section-row2 > association-container -->
       <div class="association-container w-8/12 sm:w-9/12 flex sm:flex-row flex-col gap-5 lg:gap-[96px] items-center">
         <div class="w-6/12 lg:w-8/12  flex items-center cursor-pointer">
           <img src="<?php echo $heroku_association1_img_url; ?>" alt="district-league-team-photo">
@@ -86,40 +68,13 @@ $heroku_association4_img_url = get_option('heroku_association4_img_url');
           <img src="<?php echo $heroku_association4_img_url ?>">
         </div>
       </div>
-
+      <!-- heroku-section-row2 > association-container ends here -->
     </div>
-
-
+    <!-- heroku-section-row2 ends here -->
   </div>
-
+  <!-- heroku-section ends here-->
 </main>
-
-<!-- 
-<div class="w-full h-[100px] flex items-center"><img
-            src="<?php echo get_template_directory_uri(); ?>/src/assets/images/aiff_logo.png"
-            alt="district-league-team-photo" height="100"></div>
-        <div class="w-full h-[100px] flex items-center"><img
-            src="<?php echo get_template_directory_uri(); ?>/src/assets/images/ghaziabad_fa_logo.jpg" height="100">
-        </div>
-        <div class="w-full h-[100px] flex items-center"><img
-            src="<?php echo get_template_directory_uri(); ?>/src/assets/images/logo_upfs.jpg" height="100">
-        </div>
-        <div class="w-full h-[100px] flex items-center"><img
-            src="https://www.firstkickschoolofsoccer.com/wp-content/uploads/2023/05/fkss-logo-93x93.png" height="100">
-        </div> -->
-
-
-
-
-
-
-
-
-
-
-
-
-
+<!-- ********* main-section ends here ********* -->
 <?php
 get_footer()
 ?>
