5f51474601aadab361d4315d489a3765ead0227c
Amandeep Singh <samandeep8057@gmail.com>
2024-07-16 13:03:25 +0530

    Contact-us page build, Using a plugin called wpforms, and another plugin for setting up smtp configurations called fluent smtp and finally used elementor page builder to lay things out

--- wp-content/themes/thesecondkick/page.php
+++ wp-content/themes/thesecondkick/page.php
@@ -16,23 +16,13 @@ get_header();
 ?>
 
 <main id="primary" class="site-main">
-  <h1>Page.php (Its served when wp is set to serve static page and front-page does not exists)</h1>
-  <?php
-		while ( have_posts() ) :
-			the_post();
-
-			// get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+  <?php while ( have_posts() ) :?>
+  <?php the_post();  ?>
+  <h2 class="pt-10 sm:text-5xl  sm:font-black text-3xl font-bold text-center "><?php the_title(); ?></h2>
+  <?php the_content(); ?>
+  <?php endwhile; // End of the loop. ?>
 </main><!-- #main -->
 
 <?php
-get_sidebar();
+// get_sidebar();
 get_footer();
