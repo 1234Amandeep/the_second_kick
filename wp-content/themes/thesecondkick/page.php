a8627e1efa629ea2b826553879d940e20b4cfba5
Amandeep Singh <samandeep8057@gmail.com>
2024-07-17 18:45:17 +0530

    Build Team page using elementor, still not done some additions as well as adjustments are yet to be made

--- wp-content/themes/thesecondkick/page.php
+++ wp-content/themes/thesecondkick/page.php
@@ -18,9 +18,41 @@ get_header();
 <main id="primary" class="site-main">
   <?php while ( have_posts() ) :?>
   <?php the_post();  ?>
-  <h2 class="pt-10 sm:text-5xl  sm:font-black text-3xl font-bold text-center "><?php the_title(); ?></h2>
+  <!-- <h2 class="pt-10 sm:text-5xl  sm:font-black text-3xl font-bold text-center ">/h2> -->
+  <h2 class="pt-10 sm:text-5xl  sm:font-black text-3xl font-bold text-center mb-10"><?php the_title(); ?></h2>
+
   <?php the_content(); ?>
   <?php endwhile; // End of the loop. ?>
+
+  <!-- creating team card -->
+  <!-- <section class="core-team bg-slate-200 pt-10 pb-10 mb-10">
+    <h3 class=" sm:text-3xl  sm:font-bold text-3xl font-bold text-center ">Core Team</h3>
+    <div class="core-team-inner-container container flex justify-between pt-10">
+
+      <article class="member-card flex flex-col items-center">
+        <div class="member-img">
+          <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/contact/G.png" alt="member-pic"
+            width="290">
+        </div>
+        <div class="member-cred flex flex-col items-center">
+          <h4 class="member-name">Abhishek Yadav</h4>
+          <p class="member-designation">CEO</p>
+        </div>
+
+      </article>
+      <article class="member-card flex flex-col items-center">
+        <div class="member-img">
+          <img src="<?php echo get_template_directory_uri(); ?>/src/assets/images/contact/G.png" alt="member-pic"
+            width="290">
+        </div>
+        <div class="member-cred flex flex-col items-center">
+          <h4 class="member-name">Abhishek Yadav</h4>
+          <p class="member-designation">CEO</p>
+        </div>
+
+      </article>
+    </div>
+  </section> -->
 </main><!-- #main -->
 
 <?php
